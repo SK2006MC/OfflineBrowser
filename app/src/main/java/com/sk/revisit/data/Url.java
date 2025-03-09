0295869529ba6b7efb9d2dd31198c48acd64f39f
Guru <v.sivakumaran2006@gmail.com>
2025-03-09 16:39:16 +0530

    ok

--- app/src/main/java/com/sk/revisit/data/Url.java
+++ app/src/main/java/com/sk/revisit/data/Url.java
@@ -1,58 +1,74 @@
 package com.sk.revisit.data;
 
 public class Url {
-	private final String url;
-	public long size;
-	public int progress = 0;
-	private boolean isDownloaded;
-	private boolean isSelected;
-	private boolean isUpdateAvailable;
+    private final String url;
+    private long size;
+    private int progress;
+    private boolean isDownloaded;
+    private boolean isSelected;
+    private boolean isUpdateAvailable;
+    private progressListener listener;
 
-	public Url(String url) {
-		this.url = url;
-	}
+    public Url(String url) {
+        this.url = url;
+        this.progress = 0; // Initialize progress to 0
+    }
 
-	public String getUrl() {
-		return this.url;
-	}
+    public String getUrl() {
+        return this.url;
+    }
 
-	public boolean isDownloaded() {
-		return this.isDownloaded;
-	}
+    public boolean isDownloaded() {
+        return this.isDownloaded;
+    }
 
-	public void setDownloaded(boolean downloaded) {
-		this.isDownloaded = downloaded;
-	}
+    public void setDownloaded(boolean downloaded) {
+        this.isDownloaded = downloaded;
+    }
 
-	public boolean isSelected() {
-		return isSelected;
-	}
+    public boolean isSelected() {
+        return isSelected;
+    }
 
-	public void setSelected(boolean selected) {
-		isSelected = selected;
-	}
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+    }
 
-	public boolean isUpdateAvailable() {
-		return isUpdateAvailable;
-	}
+    public boolean isUpdateAvailable() {
+        return isUpdateAvailable;
+    }
 
-	public void setUpdateAvailable(boolean updateAvailable) {
-		isUpdateAvailable = updateAvailable;
-	}
+    public void setUpdateAvailable(boolean updateAvailable) {
+        isUpdateAvailable = updateAvailable;
+    }
 
-	public long getSize() {
-		return size;
-	}
+    public long getSize() {
+        return size;
+    }
 
-	public void setSize(int size) {
-		this.size = size;
-	}
+    public void setSize(long size) {
+        this.size = size;
+    }
 
-	public void setProgressListener(progressListener listener) {
 
-	}
+    public int getProgress() {
+        return progress;
+    }
 
-	public interface progressListener {
-		void onProgressChanged(int p);
-	}
+    public void setProgress(int progress) {
+        if (progress >= 0 && progress <= 100) {
+            this.progress = progress;
+            if (listener != null) {
+                listener.onProgressChanged(progress);
+            }
+        }
+    }
+
+    public void setProgressListener(progressListener listener) {
+        this.listener = listener;
+    }
+
+    public interface progressListener {
+        void onProgressChanged(int progress);
+    }
 }
